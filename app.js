3494924dc0acdf98fcc5abd59d70c9b63bb58396
Marinho <kauegorgone@gmail.com>
2024-12-18 10:25:12 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2})
+}
+//rate = define a velocidade da voz
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosDaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosDaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+//INCLUDES = verificar se o elemento está na lista (Também é uma função)
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
+}
+
+//PUSH = coloca o item incluido ao final da lista
+//POP = remover o último elemento da lista
+
+//setAttribute = desativa o botão "Reiniciar" ao iniciar o jogo
+//removeAttribute = Ativa o botão somente após o jogador acertar o número secreto
+//Método includes verifica se o número já está no array. Se o número estiver na lista, ele chama para gerar outro número
+
+//Função = trecho de código responsável por uma ação
+//Cada vez que a função é chamada com um novo parametro, o valor do parametro é alterado
+
+//return = a função executa e retorna um valor esperado, que é armazenado na variavel quando a função é chamada
+
+
+
